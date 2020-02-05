e7b5e69a5ca610279e53b2c9cc1535a9f428c885
Vicharm <vic010744@gmail.com>
2020-02-05 16:06:42 +0800

    update e2e testing

--- cypress/integration/todo/todo.spec.js
+++ cypress/integration/todo/todo.spec.js
@@ -1,6 +1,7 @@
 describe('todo list test start', function () {
+  const serverURL = Cypress.env('api_server') || 'localhost:3000'
   it('visit web page', function () {
-    cy.visit('localhost:3000')
+    cy.visit(serverURL)
     cy.get('button').contains('新增')
   })
 })
